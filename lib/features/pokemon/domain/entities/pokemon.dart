1d7f7e237d742223394c5170871485644244c4bb
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2025-10-29 10:39:33 -0400

    feat: add pokemon class

---
+++ lib/features/pokemon/domain/entities/pokemon.dart
@@ -0,0 +1,7 @@
+class Pokemon {
+
+  final String id;
+  final String name;
+
+  Pokemon({required this.id, required this.name});
+}
---
+++ lib/features/pokemon/domain/entities/pokemon_types.dart
@@ -0,0 +1,23 @@
+
+enum PokemonTypes {
+  grass,
+  poison,
+  fire,
+  flying,
+  water,
+  bug,
+  normal,
+  electric,
+  ground,
+  fairy,
+  fighting,
+  psychic,
+  rock,
+  steel,
+  ice,
+  ghost,
+  dragon,
+  dark,
+  monster,
+  unknown,
+}
