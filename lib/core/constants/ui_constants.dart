6bb42e0f09c89dd0a294ba5c1855e135fb4e762c
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-11-09 17:21:55 -0400

    feat: add core utilities for responsive design and pokemon type styling

---
+++ lib/core/constants/ui_constants.dart
@@ -0,0 +1,31 @@
+class PokemonCardConstants {
+  static const double cardElevation = 8.0;
+  static const double backgroundOpacity = 0.2;
+  static const double errorIconSize = 48.0;
+  static const double gradientOpacity = 0.15;
+  static const double imagePositionLeft = 0.02;
+  static const double imagePositionBottom = -0.002;
+  static const double idBadgePositionRight = 0.02;
+  static const double heartIconTop = 0.02;
+  static const double heartIconRight = 0.02;
+}
+
+class App {
+  static const double appBarElevation = 8.0;
+}
+
+class PokemonIdBadgeConstants {
+  static const int idPadLength = 3;
+  static const double opacityNormal = 0.6;
+  static const double opacityLarge = 0.15;
+}
+
+class PokemonNameConstants {
+  static const double spacing = 4.0;
+}
+
+class PokemonTypeBadgeConstants {
+  static const double iconPadding = 4.8;
+  static const double spacing = 4.0;
+}
+
---
+++ lib/core/utils/pokemon_type_icons.dart
@@ -0,0 +1,7 @@
+import 'package:pokedex/features/pokemon/domain/entities/pokemon_types.dart';
+
+class PokemonTypeIcons {
+  static String getIconPath(PokemonTypes type) {
+    return 'assets/icons/types/${type.name.toLowerCase()}.svg';
+  }
+}
