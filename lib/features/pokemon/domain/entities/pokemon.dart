28649374fb6fa90f6d58e130c462c1c687a8ef9b
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-11-09 17:22:23 -0400

    refactor: convert pokemon imageUrl from computed property to field

--- lib/features/pokemon/domain/entities/pokemon.dart
+++ lib/features/pokemon/domain/entities/pokemon.dart
@@ -5,26 +5,26 @@ import 'pokemon_types.dart';
 class Pokemon extends Equatable {
   final int id;
   final String name;
+  final String? imageUrl;
   final int? height;
   final int? weight;
   final int? baseExperience;
   final List<PokemonTypes> types;
   final List<PokemonAbility>? abilities;
-  
+
   const Pokemon({
     required this.id,
     required this.name,
     required this.types,
+    this.imageUrl,
     this.height,
     this.weight,
     this.baseExperience,
     this.abilities,
   });
-  
-  String get imageUrl => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$id.png';
-  
+
   String get displayName => name[0].toUpperCase() + name.substring(1);
-  
+
   @override
   List<Object?> get props => [id, name, height, weight, baseExperience, types, abilities];
 }
