c51dbe63b3db1e9f6e5a60e1b3a8a1d47d360692
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-11-10 13:30:40 -0400

    refactor(theme): remove unused colorScheme from MaterialApp
    - Remove ColorScheme.fromSeed as it was not being used
    - Keep useMaterial3 enabled

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,6 @@ class PokedexApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const PokemonListPage(),
