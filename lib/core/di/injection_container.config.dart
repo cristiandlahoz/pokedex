50863d35a113d15924b73b2f67dfa9d4ed435c4b
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-12-12 19:36:00 -0400

    build(di): regenerate dependency injection configuration
    - Register TypeEffectivenessCalculator and Logger providers
    - Update BLoC factories to use repository directly without use cases
    - Update import paths for renamed home files

--- lib/core/di/injection_container.config.dart
+++ lib/core/di/injection_container.config.dart
@@ -11,22 +11,20 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
+import 'package:pokedex/core/di/injection_container.dart' as _i416;
 import 'package:pokedex/core/graphql/graphql_config.dart' as _i1008;
 import 'package:pokedex/core/graphql/graphql_service.dart' as _i403;
+import 'package:pokedex/core/logging/logger.dart' as _i1052;
 import 'package:pokedex/features/pokemon/bloc/details_bloc.dart' as _i477;
-import 'package:pokedex/features/pokemon/bloc/list_bloc.dart' as _i524;
+import 'package:pokedex/features/pokemon/bloc/home_bloc.dart' as _i314;
 import 'package:pokedex/features/pokemon/data/datasources/pokemon_graphql_datasource.dart'
     as _i716;
 import 'package:pokedex/features/pokemon/data/repositories/pokemon_repository_impl.dart'
     as _i337;
 import 'package:pokedex/features/pokemon/domain/repositories/pokemon_repository.dart'
     as _i896;
-import 'package:pokedex/features/pokemon/domain/usecases/get_pokemon_details.dart'
-    as _i261;
-import 'package:pokedex/features/pokemon/domain/usecases/get_pokemon_list.dart'
-    as _i680;
-import 'package:pokedex/features/pokemon/domain/usecases/search_pokemon.dart'
-    as _i1058;
+import 'package:pokedex/features/pokemon/domain/services/type_effectiveness_calculator.dart'
+    as _i538;
 
 extension GetItInjectableX on _i174.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -35,6 +33,11 @@ extension GetItInjectableX on _i174.GetIt {
     _i526.EnvironmentFilter? environmentFilter,
   }) {
     final gh = _i526.GetItHelper(this, environment, environmentFilter);
+    final loggingModule = _$LoggingModule();
+    gh.factory<_i538.TypeEffectivenessCalculator>(
+      () => _i538.TypeEffectivenessCalculator(),
+    );
+    gh.lazySingleton<_i1052.Logger>(() => loggingModule.provideLogger());
     gh.lazySingleton<_i1008.GraphQLConfig>(() => _i1008.GraphQLConfig());
     gh.lazySingleton<_i403.GraphQLService>(
       () => _i403.GraphQLService(gh<_i1008.GraphQLConfig>()),
@@ -43,26 +46,19 @@ extension GetItInjectableX on _i174.GetIt {
       () => _i716.PokemonGraphQLDataSource(gh<_i403.GraphQLService>()),
     );
     gh.lazySingleton<_i896.PokemonRepository>(
-      () => _i337.PokemonRepositoryImpl(gh<_i716.PokemonGraphQLDataSource>()),
-    );
-    gh.lazySingleton<_i261.GetPokemonDetails>(
-      () => _i261.GetPokemonDetails(gh<_i896.PokemonRepository>()),
-    );
-    gh.lazySingleton<_i680.GetPokemonList>(
-      () => _i680.GetPokemonList(gh<_i896.PokemonRepository>()),
-    );
-    gh.lazySingleton<_i1058.SearchPokemon>(
-      () => _i1058.SearchPokemon(gh<_i896.PokemonRepository>()),
-    );
-    gh.factory<_i524.ListBloc>(
-      () => _i524.ListBloc(
-        getPokemonList: gh<_i680.GetPokemonList>(),
-        searchPokemon: gh<_i1058.SearchPokemon>(),
+      () => _i337.PokemonRepositoryImpl(
+        gh<_i716.PokemonGraphQLDataSource>(),
+        gh<_i1052.Logger>(),
       ),
     );
     gh.factory<_i477.DetailsBloc>(
-      () => _i477.DetailsBloc(getPokemonDetails: gh<_i261.GetPokemonDetails>()),
+      () => _i477.DetailsBloc(repository: gh<_i896.PokemonRepository>()),
+    );
+    gh.factory<_i314.ListBloc>(
+      () => _i314.ListBloc(repository: gh<_i896.PokemonRepository>()),
     );
     return this;
   }
 }
+
+class _$LoggingModule extends _i416.LoggingModule {}
